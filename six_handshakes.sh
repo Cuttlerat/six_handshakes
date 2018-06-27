e7489495076c30abdaf5c4a359be9779af643504
cuttlerat <avkioller@gmail.com>
2018-06-27 22:56:30 +0300

    Beautified output

--- six_handshakes.sh
+++ six_handshakes.sh
@@ -7,15 +7,29 @@ readonly token="$(cat .token)"
 
 get_id() {
 
-    # $1 - URL to get id
+    # $1 - URL to get ID
     local url="${1}"
     curl -s "https://api.vk.com/method/users.get?user_ids=${url}&access_token=${token}&v=5.80" | jq .response[0].id
+}
+
+get_info() {
+
+    # $1 - ID to get info
+    local url="${1}"
+    curl -s "https://api.vk.com/method/users.get?user_id=${url}&access_token=${token}&v=5.80" |\
+        jq -r '[.response[0].first_name,.response[0].last_name] | "\(.[0]) \(.[1])"'
+}
+
+read_from_pipe() {
+
+    read "$@" <&0
 }
 
 main() {
     # $1 - First user url
     # $2 - Second user url
     local first_url="${1}" second_url="${2}"
+    local first_id second_id dir current_raw_id current_id array result
     
     first_id="$(get_id "${first_url}")"
     second_id="$(get_id "${second_url}")"
@@ -35,9 +49,13 @@ main() {
                 cd - &>/dev/null
             fi
 
-                if find . -type d -name "${second_id}" | grep '.*'; then
-                    return
-                fi
+            result=($(find . -type d -name "${second_id}" | grep '.*' | sed 's|\./||;s|/| |g' ))
+            if [[ "${result[@]}" ]]; then
+                for id in "${result[@]}"; do 
+                    get_info "${id}"
+                done | nl -s ". " | sed 's/^ *//'
+                return
+            fi
                 
         done
      done
